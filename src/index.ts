b640626dbea85d24a8cab1d5c3e1201cea59866e
Fabien Arcellier <farcellier@octo.com>
2020-04-05 16:57:46 +0200

    specify the configuration file to use

--- src/index.ts
+++ src/index.ts
@@ -10,5 +10,6 @@ const server = new ApolloServer({ typeDefs, resolvers});
 
 // The `listen` method launches a web server.
 server.listen().then(({ url }) => {
+  
   console.log(`🚀  Server ready at ${url}`);
 });
